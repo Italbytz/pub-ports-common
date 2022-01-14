fe831d727725cc7f3ab37578b8077a32b1f193b5
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-01-14 12:11:12 +0100

    0.0.2

--- lib/src/crud_repository.dart
+++ lib/src/crud_repository.dart
@@ -1,4 +1,8 @@
 import 'package:common_ports/common_ports.dart';
 
 /// A [CrudRepository] represents a data storage.
-abstract class CrudRepository implements Repository, DataSink, DataSource {}
+abstract class CrudRepository<IdType, EntityType>
+    implements
+        Repository,
+        DataSink<IdType, EntityType>,
+        DataSource<IdType, EntityType> {}
