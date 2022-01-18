c6c34e331e428b417b8a14ca91d7dd68c39be0e3
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-01-18 11:11:03 +0100

    0.0.5

--- test/common_ports_test.dart
+++ test/common_ports_test.dart
@@ -4,14 +4,14 @@ import 'package:common_ports/common_ports.dart';
 
 void main() {
   test('Command', () async {
-    var result = await MockCommand().execute(inDTO: "Question");
+    var result = await MockCommand().execute("Question");
     expect(result, 42);
   });
 }
 
 class MockCommand implements Command<String, int> {
   @override
-  Future<int> execute({String inDTO}) async {
+  Future<int> execute(String inDTO) async {
     await Future.delayed(Duration(milliseconds: 100), () {});
     return 42;
   }
